3f40f28341f0ec8228fe775b2839a3f55de3d5ad
Saheen Alam Shuvo <saheenshuvo182@gmail.com>
2025-10-30 04:31:53 +0200

    Dora and Search

--- C_Dora_and_Search.cpp
+++ C_Dora_and_Search.cpp
@@ -1,40 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-
     int t;
     cin >> t;
-
     while (t--) {
         int n;
         cin >> n;
-        vector<int> a(n);
-        for (int i = 0; i < n; i++) cin >> a[i];
-
-        int l = -1, r = -1;
-
-        // We just need to find a triplet (a[i-1], a[i], a[i+1])
-        // where the middle element is not smallest or largest
-        for (int i = 1; i + 1 < n; i++) {
-            int x = a[i - 1], y = a[i], z = a[i + 1];
-
-            if ((y > x && y > z) || (y < x && y < z)) {
-                // Middle is extreme → bad
-                continue;
-            } else {
-                // Found a valid one
-                l = i;       // 1-based
-                r = i + 2;
+        vector<int> v(n);
+        for (int i = 0; i < n; i++) cin >> v[i];
+        int f = 0, e = n - 1;
+        multiset<int> s(v.begin(), v.end());
+        while (f < e) {
+            int mn = *s.begin();
+            int mx = *s.rbegin();
+            if (v[f] == mn || v[f] == mx) {
+                s.erase(s.find(v[f]));
+                f++;
+            }
+            else if (v[e] == mn || v[e] == mx) {
+                s.erase(s.find(v[e]));
+                e--;
+            }
+            else {
                 break;
             }
         }
-
-        if (l == -1) cout << -1 << "\n";
-        else cout << l << " " << r << "\n";
+        if (f >= e) cout << -1 << "\n";
+        else cout << f + 1 << " " << e + 1 << "\n";
     }
-
     return 0;
 }
